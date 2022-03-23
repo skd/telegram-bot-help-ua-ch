48e1c0d3d988859f6ed3b3e6583897edb722ffcb
crdev <pavlov81@gmail.com>
2022-03-23 21:16:31 +0100

    Fix logging of leaf conversation nodes in stats (#43)
    Drive-by:
    - Improve stats output (1d -> 24h), add bot start time and uptime
    - Fix an error-prone default function param value

--- stats.py
+++ stats.py
@@ -1,12 +1,14 @@
 from typing import Dict
 from collections import Counter
+from pytz import timezone
 import hashlib
 import datetime
 
 HOUR = datetime.timedelta(hours=1).total_seconds()
 THREEHOURS = datetime.timedelta(hours=3).total_seconds()
 DAY = datetime.timedelta(days=1).total_seconds()
-
+BOT_TIMEZONE = timezone("Europe/Zurich")
+STARTTIME_UTC = datetime.datetime.now(BOT_TIMEZONE)
 
 class Stats:
     timestamp_by_user: Dict[str, float]
@@ -38,5 +40,7 @@ class Stats:
             daily += 1 if user_ts > now - DAY else 0
 
         node_stats = "\n".join([f"\t- {n}: {c}" for n, c in self.interactions.most_common(10)])
-        user_stats = f"\t- 1h: {hourly}\n\t- 3h: {threehourly}\n\t- 1d: {daily}"
-        return f"Total users:\n{user_stats}\nTop 10 interactions:\n{node_stats}"
+        user_stats = f"\t- 1h: {hourly}\n\t- 3h: {threehourly}\n\t- 24h: {daily}"
+        now_tz = datetime.datetime.now(BOT_TIMEZONE)
+        uptime = str(now_tz - STARTTIME_UTC).split('.')[0]
+        return f"Start time: {now_tz.replace(microsecond=0)}\nUptime: {uptime}\nTotal users:\n{user_stats}\nTop 10 interactions:\n{node_stats}"
