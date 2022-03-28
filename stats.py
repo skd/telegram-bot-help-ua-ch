4b8d4c73c69b5d057a0debac6809cd55cd7b1b25
Alexander Pavlov <pavlov81@gmail.com>
2022-03-28 13:21:39 +0200

    Address comments

--- stats.py
+++ stats.py
@@ -10,17 +10,19 @@ DAY = datetime.timedelta(days=1).total_seconds()
 BOT_TIMEZONE = timezone("Europe/Zurich")
 STARTTIME_TZ = datetime.datetime.now(BOT_TIMEZONE)
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+TOP_K_INTERACTIONS = 20
 
 class Stats:
     timestamp_by_user: Dict[str, float]
     interactions: Counter
     last_reload_time_tz: datetime.datetime
+    last_reloader_username: str
 
     def __init__(self):
         self.timestamp_by_user = dict()
         self.interactions = Counter()
         self.last_reload_time_tz = None
-        self.reloader_username = None
+        self.last_reloader_username = None
 
     def hash_user(self, user_id: int) -> str:
         return hashlib.sha256(user_id.to_bytes(10, byteorder='big', signed=True)).hexdigest()
@@ -34,7 +36,7 @@ class Stats:
 
     def conversation_reloaded(self, username):
         self.last_reload_time_tz = datetime.datetime.now(BOT_TIMEZONE)
-        self.reloader_username = username
+        self.last_reloader_username = username
 
     def compute(self) -> str:
         hourly = 0
@@ -47,7 +49,7 @@ class Stats:
             threehourly += 1 if user_ts > now - THREEHOURS else 0
             daily += 1 if user_ts > now - DAY else 0
 
-        node_stats = "\n".join([f"\t- {n}: {c}" for n, c in self.interactions.most_common(20)])
+        node_stats = "\n".join([f"\t- {n}: {c}" for n, c in self.interactions.most_common(TOP_K_INTERACTIONS)])
         user_stats = f"\t- 1h: {hourly}\n\t- 3h: {threehourly}\n\t- 24h: {daily}"
         now_tz = datetime.datetime.now(BOT_TIMEZONE)
         uptime = now_tz - STARTTIME_TZ
@@ -57,9 +59,9 @@ class Stats:
             f"Uptime: {uptime}"
         ]
         if self.last_reload_time_tz:
-            output.append(f"Last conversation reload: {self.last_reload_time_tz.strftime(DATETIME_FORMAT)} ({self.reloader_username})")
-        output += [
+            output.append(f"Last conversation reload: {self.last_reload_time_tz.strftime(DATETIME_FORMAT)} ({self.last_reloader_username})")
+        output.extend([
             f"Total users:\n{user_stats}",
-            f"Top 10 interactions:\n{node_stats}"
-        ]
+            f"Top {TOP_K_INTERACTIONS} interactions:\n{node_stats}"
+        ])
         return "\n".join(output)
